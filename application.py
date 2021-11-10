784be31e75a267b9fe020a1e981939e7cf11ea5b
lalbricenov <lalbricenov@gmail.com>
2021-11-10 05:30:00 +0000

    Passwords set in environment

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, redirect, request
 import pymongo
+import os
 
 app = Flask(__name__)
+dbUser = os.environ['dbUser']
+dbPass = os.environ['dbPass']
 
-client = pymongo.MongoClient("mongodb+srv://lbriceno:SjC91ombnfM1N5hI@cluster0.ykukg.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://{dbUser}:{dbPass}@cluster0.ykukg.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
 db = client.sample_airbnb
-#SjC91ombnfM1N5hI
-#lbriceno
 
 @app.route("/")
 def index():
